5e24753028754f37489778b0587e3a3d55fd3858
anmedina-arg <andres.medina.arg@gmail.com>
2023-07-05 17:15:54 -0300

    fixed up button whit scroll-behavior smooth and adding some css styles in about section

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -10,7 +10,7 @@ export default function Card({ project }: CardProps) {
   return (
     <>
       <div className="relative aspect-video cursor-pointer border-2 border-solid border-black flex flex-col items-center justify-center">
-        <h5 className="z-10">
+        <h5 className="z-10 uppercase text-grayLigthTheme ">
           {title}
         </h5>
         <Image src={image} alt='descripcion del proyecto' className="absolute h-full" />
--- src/sections/aboutSection.tsx
+++ src/sections/aboutSection.tsx
@@ -12,7 +12,9 @@ const AboutSection: React.FC<AboutSectionProps> = ({ index, scrollRefs }) => {
       id="about"
       className="flex flex-col justify-center items-center w-full h-screen"
     >
-      <h1 className="uppercase text-[5rem] text-darkBlueTheme">andres medina</h1>
+      <h1 className="uppercase text-[5rem] text-darkBlueTheme bg-grayLigthTheme m-4 text-center rounded-lg shadow-shadowNavTheme">andres medina</h1>
+      <span className="sm:hidden">hi there!</span>
+      <span>Welcome to my portfolio</span>
       <p className="p-2">{`I am a proactive and responsible person. I like new challenges, research, study and learn new things. This has led me to train as a Full Stack Developer with main stack JavaScript and Node Js. I developed my experience in the IT world at Henry's bootcamp, where I have carried out different projects, individually and as a team! In addition, I generated solid and important soft skills during his 12 years of experience as an Industrial Engineer where I led work teams, developing people in soft skills such as communication, feedback, inclusion, diversity, teamwork and continuous improvement.`}</p>
     </section>
   );
